f9a641d4bd9e33c0d383eb628e350a088bbd7090
Omar Hussein <omarelwy@hotmail.com>
2022-06-23 07:42:22 +0200

    ✨ Add trpc for client and api

--- apps/client/pages/index.vue
+++ apps/client/pages/index.vue
@@ -1,7 +1,16 @@
 <script setup lang="ts">
-console.log("hi");
+import { createTRPCClient } from "@trpc/client";
+
+import type { AppRouter } from "~~/@types";
+
+const client = createTRPCClient<AppRouter>({ url: "http://localhost:2022" });
+
+const helloResponse = await client.query("hello", { name: "omar" });
 </script>
 
 <template>
-  <h1>hi!</h1>
+  <div>
+    <h1>hi!</h1>
+    <p>Response {{ helloResponse }}</p>
+  </div>
 </template>
--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -2,6 +2,8 @@ module.exports = {
   ignorePatterns: ["/**/node_modules"],
   extends: ["prettier"],
   rules: {
+    "import/prefer-default-export": "off",
+    "import/prefer-default-export": "off",
     quotes: ["error", "double"],
     semi: ["error", "always"],
     "comma-dangle": ["error", "always-multiline"],
