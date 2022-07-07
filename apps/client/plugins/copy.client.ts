89fe8ad63887ce9e156b51bed936667b1163280e
Omar Hussein <omarelwy@hotmail.com>
2022-07-07 04:37:30 +0200

    ✨ Improve the copy plugin

--- apps/client/plugins/copy.client.ts
+++ apps/client/plugins/copy.client.ts
@@ -6,9 +6,32 @@ export type Copy = (
 export default defineNuxtPlugin(() => {
   const { $notify } = useNuxtApp();
 
+  function copyFromTextarea(input: string) {
+    const textarea = document.createElement("textarea");
+    textarea.value = input;
+
+    textarea.style.position = "fixed";
+    textarea.style.left = "-999999px";
+    textarea.style.top = "-999999px";
+    document.body.appendChild(textarea);
+
+    textarea.focus();
+    textarea.select();
+    textarea.setSelectionRange(0, 99999);
+
+    return new Promise(resolve => {
+      document.execCommand("copy");
+      textarea.remove();
+      resolve(true);
+    });
+  }
+
   const copy: Copy = async (textToCopy, successMessage) => {
     try {
-      await navigator.clipboard.writeText(textToCopy);
+      if (navigator.clipboard && window.isSecureContext)
+        navigator.clipboard.writeText(textToCopy);
+      else await copyFromTextarea(textToCopy);
+
       $notify.success(successMessage || "Copied!");
     } catch (e) {
       $notify.error("Couldn't copy for unknown reason, try again later");
